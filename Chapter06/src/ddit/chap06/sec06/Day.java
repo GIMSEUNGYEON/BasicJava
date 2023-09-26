36e1f5d4adf9860407458c571258c89bb0bd86b1
GIMSEUNGYEON <gims6521@gmail.com>
2023-09-26 14:04:43 +0900

    delete

--- Chapter06/src/ddit/chap06/sec06/Day.java
+++
@@ -1,8 +0,0 @@
-package ddit.chap06.sec06;
-
-public enum Day {
-	MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
-}
-enum Month{
-	JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOB, DEC 
-}
--- Chapter06/src/ddit/chap06/sec06/ShapeExample.java
+++
@@ -1,8 +0,0 @@
-package ddit.chap06.sec06;
-
-public class ShapeExample {
-	public static void main(String[] args) {
-		Circle c = new Circle("¿ø", 75);
-		c.getArea();
-	}
-}
